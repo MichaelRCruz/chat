6930da519c1f987564aebba317ef1bfa14deff8c
Michael Cruz <michael@michaelcruz.io>
2019-04-27 21:18:08 -0700

    introduce cloud functions

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,29 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+exports.helloWorld = functions.https.onRequest((req, res) => {
+  res.json({"greeting": "sup, yo"});
+});
+
+exports.handleHttpRedirect = functions.https.onRequest((req, res) => {
+  console.log('inside handleHttpRedirect');
+  res.redirect(303, 'https://michaelcruz.io/chat');
+});
+
+exports.insertIntoDb = functions.https.onRequest((req, res) => {
+  const text = req.query.text;
+  admin.database().ref('/test').push({text: text}).then(snapshot => {
+    console.log('snapshot.ref: ', snapshot.ref);
+    res.redirect(303, snapshot.ref);
+  });
+});
+
+exports.convertToUppercase = functions.database.ref('/test/{pushId}/text')
+  .onWrite(event => {
+    const text = event.data.val();
+    const uppercaseText = text.toUpperCase();
+    console.log('text uppercaseText', text, uppercaseText);
+    return event.data.ref.parent.child('uppercaseText').set(uppercaseText);
+;});

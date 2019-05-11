d614b58b5039beeb9c9e840fe712343e3442d04e
Michael Cruz <michael@michaelcruz.io>
2019-05-11 00:48:27 -0700

    change temp message

--- src/Input/Textarea.js
+++ src/Input/Textarea.js
@@ -28,7 +28,7 @@ export default class Textarea extends React.Component {
         />
         <label htmlFor={this.props.input.name}>
           {this.props.label}
-          {error || warning || 'The enter key has been temporarily disbaled through this stage if development.'}
+          {error || warning || 'enter is for new line only'}
         </label>
       </div>
     );

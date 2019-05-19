31623020571bc8f774c3cbf929de6192646bc758
Michael Cruz <michael@michaelcruz.io>
2019-05-19 13:55:00 -0700

    wip

--- src/App.js
+++ src/App.js
@@ -20,24 +20,22 @@ const config = {
   messagingSenderId: "145747598382"
 };
 firebase.initializeApp(config);
-var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
-if (!isSafari) {
-  const messaging = firebase.messaging();
-  messaging.requestPermission()
-    .then(function() {
-      console.log('have permission');
-      return messaging.getToken();
-    })
-    .then(function(token) {
-      console.log('message token: ', token);
-    })
-    .catch(function(err) {
-      console.log('error occured');
-    });
-  messaging.onMessage(function(payload) {
-    console.log('onMessage', payload);
-  });
-}
+
+// const messaging = firebase.messaging();
+// messaging.requestPermission()
+//   .then(function() {
+//     console.log('have permission');
+//     return messaging.getToken();
+//   })
+//   .then(function(token) {
+//     console.log('message token: ', token);
+//   })
+//   .catch(function(err) {
+//     console.log('error occured');
+//   });
+// messaging.onMessage(function(payload) {
+//   console.log('onMessage', payload);
+// });
 
 class App extends Component {
   constructor(props) {

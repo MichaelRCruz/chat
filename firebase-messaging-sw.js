eb4a2e5586956afd41eb1a9ce5a3e95c2fa89973
Michael Cruz <michael@michaelcruz.io>
2019-05-19 20:18:51 -0700

    wip

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -13,13 +13,13 @@ firebase.initializeApp(config);
 
 const messaging = firebase.messaging();
 
-// messaging.setBackgroundMessageHandler(function(payload) {
-//   const title = 'Hello, world from SW.';
-//   const options = {
-//     body: payload.data.status
-//   }
-//   return self.registration.showNotification(title, options);
-// });
+messaging.setBackgroundMessageHandler(function(payload) {
+  const title = 'Hello, world from SW.';
+  const options = {
+    body: payload.data.status
+  }
+  return self.registration.showNotification(title, options);
+});
 
 // importScripts("https://www.gstatic.com/firebasejs/5.9.4/firebase-app.js");
 // importScripts("https://www.gstatic.com/firebasejs/5.9.4/firebase-messaging.js");

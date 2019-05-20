a82b1e480bb55605e530c6e8e9276fa45ddea25e
Michael Cruz <michael@michaelcruz.io>
2019-05-20 00:21:54 -0700

    Updates

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -20,7 +20,7 @@
 //   }
 //   return self.registration.showNotification(title, options);
 // });
-
+//
 importScripts("https://www.gstatic.com/firebasejs/5.9.4/firebase-app.js");
 importScripts("https://www.gstatic.com/firebasejs/5.9.4/firebase-messaging.js");
 firebase.initializeApp({

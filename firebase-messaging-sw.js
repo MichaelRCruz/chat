8cef1a079ef7df30189163c4583c9e488fb072c0
Michael Cruz <michael@michaelcruz.io>
2019-05-20 01:17:19 -0700

    Updates

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -23,6 +23,14 @@
 //
 importScripts("https://www.gstatic.com/firebasejs/5.9.4/firebase-app.js");
 importScripts("https://www.gstatic.com/firebasejs/5.9.4/firebase-messaging.js");
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('../firebase-messaging-sw.js')
+  .then(function(registration) {
+    console.log('Registration successful, scope is:', registration.scope);
+  }).catch(function(err) {
+    console.log('Service worker registration failed, error:', err);
+  });
+}
 firebase.initializeApp({
 	// Project Settings => Add Firebase to your web app
   messagingSenderId: "145747598382"

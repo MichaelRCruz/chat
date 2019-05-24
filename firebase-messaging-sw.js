2521e7ac22877d24a79bf866541f83792a60b35c
Michael Cruz <michael@michaelcruz.io>
2019-05-24 06:16:08 -0700

    Updates

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,40 +1,7 @@
-// importScripts('https://www.gstatic.com/firebasejs/3.4.0/firebase-app.js');
-// importScripts('https://www.gstatic.com/firebasejs/3.4.0/firebase-messaging.js');
-//
-// const config = {
-//   apiKey: "AIzaSyAgvoGPD9Rh1p1Pf0TxHTdPGunB_KR9OqQ",
-//   authDomain: "chat-asdf.firebaseapp.com",
-//   databaseURL: "https://chat-asdf.firebaseio.com",
-//   projectId: "chat-asdf",
-//   storageBucket: "chat-asdf.appspot.com",
-//   messagingSenderId: "145747598382"
-// };
-// firebase.initializeApp(config);
-//
-// const messaging = firebase.messaging();
-//
-// messaging.setBackgroundMessageHandler(function(payload) {
-//   const title = 'Hello, world.';
-//   const options = {
-//     body: payload.data.status
-//   }
-//   return self.registration.showNotification(title, options);
-// });
-//
 importScripts('https://www.gstatic.com/firebasejs/4.1.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/4.1.1/firebase-messaging.js');
 importScripts('https://www.gstatic.com/firebasejs/4.1.1/firebase.js');
-// if ('serviceWorker' in navigator) {
-//   navigator.serviceWorker.register('../firebase-messaging-sw.js')
-//   .then(function(registration) {
-//     console.log('Registration successful, scope is:', registration.scope);
-//   }).catch(function(err) {
-//     console.log('Service worker registration failed, error:', err);
-//   });
-// }
-console.log('inside fms...')
 firebase.initializeApp({
-	// Project Settings => Add Firebase to your web app
   messagingSenderId: "145747598382"
 });
 const messaging = firebase.messaging();
@@ -51,7 +18,7 @@ messaging.setBackgroundMessageHandler(function(payload) {
       }
     })
     .then(() => {
-      return registration.showNotification("new message from michaelcruz.io");
+      return registration.showNotification("new mention from michaelcruz.io");
     });
   return promiseChain;
 });

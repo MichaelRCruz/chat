65d3a529482a838930830b50faf766f12936f090
Michael Cruz <michael@michaelcruz.io>
2019-05-27 18:39:32 -0700

    style splash page generically

--- src/Splash/Splash.js
+++ src/Splash/Splash.js
@@ -8,14 +8,15 @@ const Splash = ({toggleModal}) => {
     <div className="splashComponent">
       <header className="splashHeader">
         <div className="headerContentContainer">
-          <div className="nameLogoContainer">
-            <img className="splashLogo" src={require("../assets/images/potato2.svg")}
-                 alt="potato logo"
-                 onClick={toggleModal}
-            />
-            <p className="appName">Potato</p>
-          </div>
-          <i class="material-icons menuIcon">menu</i>
+          <a className="anchor" href="https://michaelcruz.io/chat">
+            <div className="nameLogoContainer">
+              <img className="splashLogo" src={require("../assets/images/potato2.svg")}
+                   alt="potato logo"
+              />
+              <p className="appName">Potato</p>
+            </div>
+          </a>
+          <i onClick={toggleModal} class="material-icons menuIcon">menu</i>
         </div>
       </header>
       <main className="splashMain">
@@ -24,13 +25,21 @@ const Splash = ({toggleModal}) => {
             Live chat with anyone, simplified.
           </h1>
           <p className="appDescription">
-            Lorem ipsum dolor amet iPhone green juice squid, fam yr ennui bespoke banh mi cornhole. Health goth taxidermy.
+            Welcome to your new live chat application! Share your thoughts and ideas with anybody. Click the link below to get started.
           </p>
-          <button className="getStartedButton">GET STARTED NOW</button>
-          <img width="109" alt="" src="https://assets-global.website-files.com/5c991ff59b4c11294d51a8bd/5c991ff59b4c11602151a923_Google.svg" />
+          <button className="getStartedButton" onClick={toggleModal}>GET STARTED</button>
+          <div className="companyImagesContainer">
+            <img className="google" alt="" src={"https://assets-global.website-files.com/5c991ff59b4c11294d51a8bd/5c991ff59b4c11602151a923_Google.svg"} />
+          </div>
         </div>
       </main>
-      <footer className="splashFooter">This is footer</footer>
+      <footer className="splashFooter">
+        <ul>
+          <li><a href="https://michaelcruz.io">michaelcruz.io</a></li>
+          <li><a href="https://www.linkedin.com/in/michaelrcruz/">linkedin</a></li>
+          <li><a href="https://www.instagram.com/mikeyscript/">instagram</a></li>
+        </ul>
+      </footer>
     </div>
   );
 };

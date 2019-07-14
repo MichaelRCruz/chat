d36abed625c0a86ca6d121678227e5ef96b1a2ce
Michael Cruz <michael@michaelcruz.io>
2019-07-14 00:57:51 -0700

    wip

--- src/Splash/Splash.js
+++ src/Splash/Splash.js
@@ -16,43 +16,35 @@ class Splash extends React.Component  {
     return (
       <div className="splashComponent">
         <header className="splashHeader">
-          <div className="headerContentContainer">
-            <a className="anchor" href="https://michaelcruz.io/chat">
-              <div className="nameLogoContainer">
-                <img className="splashLogo" src={require("../assets/images/potato2.svg")}
-                     alt="potato logo"
-                />
-                <p className="splashAppName">Potato</p>
-              </div>
-            </a>
-            <Link to={`/signIn`}>
-              <i className="material-icons menuIcon">menu</i>
-            </Link>
-          </div>
+          <Link className="anchor" to={'/'}>
+            <div className="nameLogoContainer">
+              <img className="splashLogo" src={require("../assets/images/potato2.svg")}
+                   alt="potato logo"
+              />
+              <p className="splashAppName">Potato</p>
+            </div>
+          </Link>
+          <Link to={`/auth/registration`}>
+            <i className="material-icons menuIcon">menu</i>
+          </Link>
         </header>
         <main className="splashMain">
-          <div className="mainContentContainer">
+          <section className="mainContentContainer">
             <h1 className="hero">
               Live chat with anyone, simplified.
             </h1>
             <p className="appDescription">
               Welcome to your new live chat application! Share your thoughts and ideas with anybody, anywhere. Click the link below to get started.
             </p>
-            <button className="getStartedButton" onClick={this.redirectToSignIn}>GET STARTED</button>
-          </div>
-          <div className="companyImagesContainer">
+            <Link to={`/auth/registration`}>
+              GET STARTED
+            </Link>
+          </section>
+          <section className="companyImagesContainer">
             <img className="firebaseLogo" alt="" src={require("../assets/Built_with_Firebase_Logo_Light.svg")} />
             <img className="reactLogo" alt="" src={require("../assets/react_logo.svg")} />
-            <img className="githubPagesLogo" alt="" src={require("../assets/github_pages.png")} />
-          </div>
+          </section>
         </main>
-        <footer className="splashFooter">
-          <ul>
-            <li><a href="https://michaelcruz.io">michaelcruz.io</a></li>
-            <li><a href="https://www.linkedin.com/in/michaelrcruz/">linkedin</a></li>
-            <li><a href="https://www.instagram.com/mikeyscript/">instagram</a></li>
-          </ul>
-        </footer>
       </div>
     )
   };
